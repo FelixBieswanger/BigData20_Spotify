5873b2249aeada194aca7fae5b9fe0c66acc112d
FelixBieswanger <felix.bieswanger@googlemail.com>
2020-06-12 13:53:02 +0200

    implement wait on kafka

--- mvp/resources/kafka_factory.py
+++ mvp/resources/kafka_factory.py
@@ -1,16 +1,40 @@
 from kafka import KafkaConsumer, KafkaProducer
+import time
 
 class Kafka_factory:
     def get_kafka_util(self,util):
-        if util is None:
-            return None
         if util == "consumer":
-            consumer = KafkaConsumer("dbtest",\
-                bootstrap_servers="localhost:9092",\
-                value_deserializer=lambda x: json.loads(x.decode('utf-8')),\
-                auto_offset_reset="latest")
-            return consumer
+            consumer = None
+            retrys = 0
+            while consumer == None and retrys <= 5:
+                try:
+                    consumer = KafkaConsumer("dbtest",\
+                        bootstrap_servers="localhost:9092",\
+                        value_deserializer=lambda x: json.loads(x.decode('utf-8')),\
+                        auto_offset_reset="latest")
+                    return consumer
+                except:
+                    retrys+=1
+                    time.sleep(3)
+                
+                print("Could not connect to kafka server")
+                return None
+
         if util == "poducer":
-            producer = KafkaProducer(bootstrap_servers='localhost:9092',
-                                     value_serializer=lambda x: json.dumps(x).encode("ascii"))
-            return producer
+            producer = None
+            retrys = 0
+            while producer == None and retrys <= 5:
+                try:
+                    producer = KafkaProducer(bootstrap_servers='localhost:9092',
+                                            value_serializer=lambda x: json.dumps(x).encode("ascii"))
+                    return producer
+                except:
+                    retrys += 1
+                    time.sleep(3)
+
+            print("Could not connect to kafka server")
+            return None
+        
+        return None
+            
+           
