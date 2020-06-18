d1f761c81106176413d23846c5b2e89df5d15701
FelixBieswanger <felix.bieswanger@googlemail.com>
2020-06-18 09:51:16 +0200

    bug fix

--- mvp/artists/artists.py
+++ mvp/artists/artists.py
@@ -2,11 +2,10 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-import json
 from resources.spotify_auth import Spotify_Auth
 from resources.kafka_factory import Kafka_factory
-import time
+import json
+from datetime import datetime
 
 
 auth = Spotify_Auth()
@@ -16,35 +15,39 @@ kafka_factory = Kafka_factory()
 consumer = kafka_factory.get_util("consumer")
 producer = kafka_factory.get_util("producer")
 
+last_send = datetime.now()
+
 
 def do_nice_stuff(track_art_map,artist_store):
 
-    artists_string = ",".join(artist_store)
-    params = {
-        "ids": artists_string
-    }
+    if len(artist_store) > 0:
+        artists_string = ",".join(artist_store)
+        params = {
+            "ids": artists_string
+        }
+        print("sending request with",len(artist_store),"artists")
+        response = auth.get("https://api.spotify.com/v1/artists/", params=params)
+        last_send = datetime.now()
+        if response.status_code == 200:
+            result = json.loads(response.content)
+            #create artists + artist-track relationship
+            for artist in result["artists"]:
+                for trackid in track_art_map[artist["id"]]:
+                    message = {
+                        "id": artist["id"],
+                        "name": artist["name"],
+                        "popularity": artist["popularity"],
+                        "followers": artist["followers"]["total"],
+                        "genres": ",".join(artist["genres"]),
+                        "trackid":trackid
+                    }
+                    producer.send("createArtistandRel", value=message)
+            producer.flush()
 
-    print("sending request with",len(artist_store),"artists")
-    response = auth.get("https://api.spotify.com/v1/artists/", params=params)
-    if response.status_code == 200:
-        result = json.loads(response.content)
-        #create artists + artist-track relationship
-        for artist in result["artists"]:
-            for trackid in track_art_map[artist["id"]]:
-                message = {
-                    "id": artist["id"],
-                    "name": artist["name"],
-                    "popularity": artist["popularity"],
-                    "followers": artist["followers"]["total"],
-                    "genres": ",".join(artist["genres"]),
-                    "trackid":trackid
-                }
-
-                producer.send("createArtistandRel", value=message)
-        producer.flush()
-
-    elif response.status_code == 429:
-        print("Too Many Requests, RATE LIMIT")
+        elif response.status_code == 429:
+            print("Too Many Requests, RATE LIMIT")
+        else:
+            print(response.status_code,"Error",response.content)
 
 track_art_map = dict()
 artist_store = list()
@@ -67,13 +70,15 @@ for message in consumer:
                     if trackid not in track_art_map[artistid]:
                         track_art_map[artistid].append(trackid)
 
-                if len(artist_store) > 45:
+                diff = datetime.now() - last_send
+                if len(artist_store) > 45 or diff.seconds == 20:
                     do_nice_stuff(track_art_map.copy(),artist_store.copy())
                     track_art_map = dict()
                     artist_store = list()
-             
 
-do_nice_stuff(track_art_map, artist_store)
+do_nice_stuff(track_art_map.copy(), artist_store.copy())
+
+
 
 
 
